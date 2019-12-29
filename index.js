49fec979858c8f95f46c3a9259ae571f55c00440
super-archive-1419 <sophzsul@gmail.com>
2019-12-29 05:48:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var a, b
+
+(function() {
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+})
+function add(a,b){
+  return a+b
+};
+function subtract(a,b){
+return a-b
+};
+function multiply(a,b){
+  return a*b
+};
+function divide (a,b){
+  return a/b
+};
+function inc(a){
+  return ++a
+};
+function dec(a){
+  return --a
+};
+function makeInt(a){
+  return a.toString
+};
+funtion makeInt('0x2328'){
+return 0
+}
